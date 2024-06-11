93d91dee096e8e8e7f3b9ec2435231cd6d0ab876
guskirb <anguskirby1998@gmail.com>
2024-06-11 07:56:35 +0100

    Add basic nav bar & form styling

--- models/user.js
+++ models/user.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    username: { type: String, required: true },
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    email: { type: String, required: true },
     password: { type: String, required: true },
     admin: { type: Boolean }
 });
--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,16 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Members Only' });
 });
 
 router.get('/signup', function(req, res, next) {
-  res.render('sign_up', { title: 'Express' });
+  res.render('sign_up', { title: 'Sign Up' });
 });
 
 
 router.get('/login', function(req, res, next) {
-  res.render('log_in', { title: 'Express' });
+  res.render('log_in', { title: 'Log In' });
 });
 
 module.exports = router;
