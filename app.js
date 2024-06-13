51e9c1eb175b4225edd9c4864491ed5d7cc7bcd1
guskirb <anguskirby1998@gmail.com>
2024-06-13 07:32:34 +0100

    Remove unused routes

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ async function main() {
 }
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -52,7 +51,6 @@ app.use((req, res, next) => {
 });
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
